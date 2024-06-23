007eb96f428ed0706c5816302e2cb0b63eac784f
Deepak Ghimire <139208927+Dipakxettri@users.noreply.github.com>
2024-06-23 13:22:18 +0545

    Create ArrayDeclarationAndInitialization.java
    Java file of array declaration and Initialization.

---
+++ ArrayPractice/ArrayDeclarationAndInitialization.java
@@ -0,0 +1,9 @@
+//Array Declaration And Initialization
+public class ArrayDeclarationAndInitialization{
+    public static void main(String[] args) {
+        int [] arry = {1,2,3,4};//Array
+        for (int i = 0;i<arry.length;i++){
+            System.out.print(arry[i]+" ");
+        }
+    }
+}
