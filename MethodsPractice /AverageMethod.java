f36e1983dba69582f5bafadaf3969671875dcbb9
Deepak Ghimire <139208927+Dipakxettri@users.noreply.github.com>
2024-06-28 13:07:15 +0545

    Create AverageMethod.java
    Calculating Average of numbers using methods.

---
+++ MethodsPractice /AverageMethod.java
@@ -0,0 +1,14 @@
+
+//Average of numbers using methods
+
+public class AverageMethod {
+    static int Average(int x,int y){
+        int l = 2;
+        int sum = x+y;
+        int average = sum/l;
+        return average;
+    }
+    public static void main(String[] args) {
+        System.out.println(Average(20,20));
+    }
+}
