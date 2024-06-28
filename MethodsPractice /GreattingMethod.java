9a2be6e52fc03b6ccbc9d6655ab442e2d55cd919
Deepak Ghimire <139208927+Dipakxettri@users.noreply.github.com>
2024-06-28 13:11:25 +0545

    Create GreattingMethod.java
    Method to Greatting a user.

---
+++ MethodsPractice /GreattingMethod.java
@@ -0,0 +1,11 @@
+
+//Greatting methods
+
+public class GreattingMethod {
+    static void Greatting(){
+        System.out.println("  Hello Good Morning!\n  Have a nice day!");
+    }
+    public static void main(String[] args) {
+        Greatting();
+    }
+}
