bf65659719a5d493ab55276125686bdf44d04780
Deepak Ghimire <139208927+Dipakxettri@users.noreply.github.com>
2024-06-28 12:44:34 +0545

    Create SumMethod.java
    The method to sum 2 numbers.

---
+++ MethodsPractice /SumMethod.java
@@ -0,0 +1,13 @@
+
+//sum of numbers using methods
+
+public class SumMethod {
+    static int sum (int x,int y){
+        int xy = x+y;
+        return xy;
+    }
+    public static void main(String[] args) {
+        System.out.println(sum(20,20));
+        
+    }
+}
