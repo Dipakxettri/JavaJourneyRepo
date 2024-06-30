1e0186143c410418bb89af1a25ffc8c72bdee1e4
Deepak Ghimire <139208927+Dipakxettri@users.noreply.github.com>
2024-06-30 15:10:55 +0545

    Create Area&PerimeterOfSquare.java
    Opps to identify the Area and Perimeter of a Square
    Formula :
    Area = side * side
    Perimeter = 4 * side

---
+++ Object-Oriented-Programming/Area&PerimeterOfSquare.java
@@ -0,0 +1,27 @@
+
+//Opps to identify the Area and Perimeter of a Square
+//Formula :
+//Area = side * side
+//Perimeter = 4 * side
+
+class Square{
+    int side;
+    
+    public int area(){
+        return side * side;
+    }
+    
+    public int perimeter(){
+        return 4 * side;
+    }
+}
+
+public class Area&PerimeterOfSquare{
+    public static void main(String[] args) {
+        
+        Square sq = new Square();
+        sq.side = 8;
+        System.out.println(sq.area());
+        System.out.println(sq.perimeter());
+    }
+}
